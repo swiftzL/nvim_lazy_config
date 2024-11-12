7d7214e51f3d47b1e37c5384ef12137931d99138
swiftzl <a@qq.com>
2024-11-12 21:02:03 +0800

    go.nvim

--- lua/plugins/myplugin.lua
+++ lua/plugins/myplugin.lua
@@ -1,1 +1,18 @@
-return {}
+return {
+
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      print("call go.nvim start")
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+}
